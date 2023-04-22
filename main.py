a91d81a916145c2ae1f8eae06377e74f14359b85
Your Name <you@example.com>
2023-04-22 16:03:01 +0300

    make edits

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+import pandas
+import collections
+import datetime
+import argparse
 
 
 def define_the_ending(num):
     last_digit = num % 10
     last_2_digits = num % 100
-    if 10 < last_2_digits < 20 or 4 < last_digit < 10 or last_digit == 0:
+    if 10 < last_2_digits < 20 or 4 < last_digit < 10 or not last_digit:
         ending = 'лет'
     elif 1 < last_digit < 5:
         ending = 'года'
@@ -15,36 +19,33 @@ def define_the_ending(num):
 
 
 def count_years_of_work():
-    import datetime
     present_year = datetime.date.today().year
     opening_year = 1920
     years_of_work = present_year - opening_year
     return years_of_work
 
 
-def read_excel():
-    import pandas
-    import collections
-    wines_inf = pandas.read_excel('wine.xlsx')\
+def read_excel(path):
+    wines = pandas.read_excel(path)\
         .fillna("")\
         .to_dict(orient='records')
     categories = collections.defaultdict(list)
-    for wine in wines_inf:
+    for wine in wines:
         categories[wine['Категория']].append(wine)
     return categories
 
 
-def update_years_of_work():
+def update_index_html(path):
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
     template = env.get_template('template.html')
     years_of_work = count_years_of_work()
-    wines_inf = read_excel()
+    categories_of_wine = read_excel(path)
 
     rendered_page = template.render(
-        wines_inf=wines_inf,
+        categories_of_wine=categories_of_wine,
         years_of_work=f'{years_of_work} {define_the_ending(years_of_work)}'
     )
 
@@ -52,7 +53,24 @@ def update_years_of_work():
         file.write(rendered_page)
 
 
-update_years_of_work()
+def main():
+    parser = argparse.ArgumentParser(
+        description='Программа согласно данным обновляет'
+                    ' файл "index.html" (если нет, создает) .'
+    )
+    parser.add_argument(
+        '--path',
+        default='wine.xlsx',
+        help='Укажите путь до файла excell с информацией о продуктах.'
+             ' Если не указать, программа дефолтно возьмет информацию'
+             ' из файла wine.xlsx, который лежит'
+             ' в одной директории с основым кодом.'
+    )
+    args = parser.parse_args()
+    update_index_html(args.path)
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+
+if __name__ == '__main__':
+    main()
